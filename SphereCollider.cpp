60b6e43a02e2c48aeb4997fc83ac85f66500ead5
yafusosyun <122337129+yafusosyun@users.noreply.github.com>
2023-08-08 09:24:33 +0900

    a

--- SphereCollider.cpp
+++ SphereCollider.cpp
@@ -1,1 +1,6 @@
 #include "SphereCollider.h"
+
+void SphereCollider::CheckCollision(class SphereCollider)
+{
+
+}
--- SphereCollider.h
+++ SphereCollider.h
@@ -2,11 +2,15 @@
 class SphereCollider
 {
 public:
-	void CheckCollision();
+	void CheckCollision(class SphereCollider);
 	//引数でSphereColliderを受け取る
 	//SphereCollider同士の当たり判定を行い当たったかどうかを返す
 
 	int location;//中心座標
-	int radius;
+	int radius;//半径
+
+	//三角関数を使って、敵と弾を円で当たり判定を作り
+	//それぞれの中心を使って距離を求め
+	//radius　敵と弾の半径を足した値より短ければ当たっている
 };
 
